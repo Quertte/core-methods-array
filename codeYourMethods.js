ea666fc3cabf76655912ee873a6bd3d7ca2eb7ea
Quertte <a.injen90@gmail.com>
2022-08-09 13:43:00 +0300

    methods...

--- codeYourMethods.js
+++ codeYourMethods.js
@@ -4,30 +4,68 @@
 // const newArr = myMap([1,2,3], (el) => el * 2);
 
 
-function myForEach() {
-
+function myForEach(array, callBack) {
+  for (let i = 0; i < array.length; i++) {
+    callBack(array[i]);
+  }
 }
 
-function myMap() {
-
+function myMap(array, callBack) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(callBack(array[i]));
+  }
+  return result;
 }
 
-function myFilter() {
-
+function myFilter(array, callBack) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callBack(array[i])) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
 
-function myFind() {
-
+function myFind(array, callBack) {
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (callBack(array[i])) {
+      result = array[i];
+      break;
+    }
+  }
+  return result;
 }
 
-function myEvery() {
-
+function myEvery(array, callBack) {
+  let result = true;
+  for (let i = 0; i < array.length; i++) {
+    if (!callBack(array[i])) {
+      result = false;
+      break;
+    }
+  }
+  return result;
 }
 
-function mySome() {
+// console.log(myEvery([1, 2, 3, 4, 5, 6, -7, 8], elem => elem > 0));
 
+function mySome(array, callBack) {
+  let result = false;
+  for (let i = 0; i < array.length; i++) {
+    if (callBack(array[i])) {
+      result = true;
+      break;
+    }
+  }
+  return result;
 }
 
-function myReduce() {
+console.log(mySome([1, 2, 3, 4, 5, 6, 7, 8], elem => elem < 0));
 
+function myReduce(array, callBack) {
+  const result = 0;
+  
 }
